75e4b0ac192a9eaf867f1ae160554926ba51c5e8
Vadzim <vadzimk@hotmail.com>
2023-02-23 02:43:36 -0800

    add home template and class view

--- pages/views.py
+++ pages/views.py
@@ -1,7 +1,5 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
-from django.http import HttpResponse
 
-def homePageView(request):
-    return HttpResponse('Hello from pages')
+class HomePageView(TemplateView):
+    template_name = 'home.html'
