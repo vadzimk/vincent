f57105b673c93318afef2b38c4a65e8721e71255
Vadzim <vadzimk@hotmail.com>
2023-02-24 03:47:42 -0800

    add blog post detail

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from .views import BlogListView
+from .views import BlogListView, BlogDetailView
 
 urlpatterns = [
-        path('', BlogListView.as_view(), name='blogs'),  # root of 'posts/'
+    path('post/<int:pk>/', BlogDetailView.as_view(), name='post_detail'), # pk is a url parameter that is passed to the post_detail in the template
+    path('', BlogListView.as_view(), name='blogs'),  # root of 'blog/'
 ]
-
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from blog.models import Post
 # Create your views here.
 
@@ -7,3 +7,9 @@ class BlogListView(ListView):
     model = Post
     template_name = 'blog.html'
     context_object_name = 'all_blogs_list'
+
+
+class BlogDetailView(DetailView):
+    model = Post
+    template_name = 'post_detail.html'
+    context_object_name = 'post_content'
