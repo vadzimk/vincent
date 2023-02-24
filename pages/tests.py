3f8d2114479fa28fc3fb5bb6059390984509ce69
Vadzim <vadzimk@hotmail.com>
2023-02-23 19:32:09 -0800

    add home and about tests

--- pages/tests.py
+++ pages/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class SimpleTests(TestCase):
+    def test_home_page_status_code(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_about_page_status_code(self):
+        self.assertEqual(self.client.get('/about/').status_code, 200)  # if  no trailing '/' will be status 301
